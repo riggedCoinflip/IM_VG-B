b49628897638ee014bce66f1986b84bd65ebae3b
riggedCoinflip <tikalker@gmail.com>
2021-02-08 21:10:57 +0100

    combine multiple else if with same intention into 1 else if (a or b or c)

--- src/main/java/de/thkoeln/inf/sysges/camunda/servletwar/starter/imstarter/versicherungsantrag/IsInsuranceAllowedDelegate.java
+++ src/main/java/de/thkoeln/inf/sysges/camunda/servletwar/starter/imstarter/versicherungsantrag/IsInsuranceAllowedDelegate.java
@@ -10,22 +10,18 @@ public class IsInsuranceAllowedDelegate implements JavaDelegate {
         int pv_bmiRisk = (int) delegateExecution.getVariable("pv_bmiRisk");
         int pv_historyRisk = (int) delegateExecution.getVariable("pv_historyRisk");
 
-        if (pv_bmiRisk > 100 || pv_historyRisk >= 100) {
+        if (pv_bmiRisk >= 100 || pv_historyRisk >= 100) {
             delegateExecution.setVariable("pv_isInsurable", false);
             delegateExecution.setVariable("pv_manualCheck", false);
-        } else if (pv_ageRisk < 50 && pv_bmiRisk < 50 && pv_historyRisk < 50) {
+        } else if ((pv_ageRisk < 50 && pv_bmiRisk < 50 && pv_historyRisk < 50) ||
+                ((pv_ageRisk < 50 && (pv_bmiRisk >= 50 && pv_bmiRisk < 70) && pv_historyRisk < 50)) ||
+                ((pv_ageRisk >= 50 && pv_ageRisk < 70) && pv_bmiRisk < 50 && pv_historyRisk < 50)) {
             delegateExecution.setVariable("pv_isInsurable", true);
             delegateExecution.setVariable("pv_manualCheck", false);
-        } else if ((pv_ageRisk < 50 && (pv_bmiRisk >= 50 && pv_bmiRisk < 70) && pv_historyRisk < 50) || ((pv_ageRisk >= 50 && pv_ageRisk < 70) && pv_bmiRisk < 50 && pv_historyRisk < 50)) {
-            delegateExecution.setVariable("pv_isInsurable", true);
-            delegateExecution.setVariable("pv_manualCheck", false);
-        } else if ((pv_ageRisk >= 50 && pv_ageRisk < 70) && (pv_bmiRisk >= 50 && pv_bmiRisk < 70)) {
-            delegateExecution.setVariable("pv_manualCheck", true);
-        } else if (pv_historyRisk >= 50) {
-            delegateExecution.setVariable("pv_manualCheck", true);
-        } else if (pv_ageRisk >= 70 && pv_historyRisk >= 25) {
-            delegateExecution.setVariable("pv_manualCheck", true);
-        } else if (pv_bmiRisk >= 50 && pv_historyRisk >= 25) {
+        } else if (((pv_ageRisk >= 50 && pv_ageRisk < 70) && (pv_bmiRisk >= 50 && pv_bmiRisk < 70)) ||
+                (pv_historyRisk >= 50) ||
+                (pv_ageRisk >= 70 && pv_historyRisk >= 25) ||
+                (pv_bmiRisk >= 50 && pv_historyRisk >= 25)) {
             delegateExecution.setVariable("pv_manualCheck", true);
         } else {
             throw new IllegalArgumentException(String.format("No insurance policy for ageRisk = %d, bmiRisk = %d, historyRisk =  %d", pv_ageRisk, pv_bmiRisk, pv_historyRisk));
